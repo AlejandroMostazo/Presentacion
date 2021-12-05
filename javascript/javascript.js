1d9584f1f676d519a1821640e93d9adc65091f23
AlejandroMostazo <91953147+AlejandroMostazo@users.noreply.github.com>
2021-12-05 13:47:47 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+/*esta funcion hace a una etiqueta visible llamandola por su id*/
+
+function mostrar(imagen) {
+    document.getElementById(imagen).style.visibility="visible";
+
+}
+
+/*esta funcion hace a una etiqueta invisible llamandola por su id*/
+
+function ocultar(imagen) {
+    document.getElementById(imagen).style.visibility="hidden";
+}
